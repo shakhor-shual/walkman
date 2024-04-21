cdcdca582f5b970efe6af1396b752d65cb2ed8cf
Vadym Yanik <vadim.yanik@gmail.com>
2024-04-21 10:24:21 +0200

    fixed podman installation

--- bin/cw4d.sh
+++ bin/cw4d.sh
@@ -497,8 +497,9 @@ init_home_local_bin() {
     echo "$PATH" | grep -q "$user_home_bin" || export PATH=$user_home_bin:$PATH
 
     if [ -z "$(which docker)" ] && [ -z "$(which podman)" ]; then
-        try_as_root curl -o "$user_home_bin"/podman-remote-static-linux_${arch}.tar.gz https://github.com/containers/podman/releases/download/v${PODMAN_v}/podman-remote-static-linux_${arch}.tar.gz
-        try_as_root tar -zxvf podman-remote-static-linux_${arch}.tar.gz -C "$user_home_local" bin/podman-remote-static-linux_${arch}
+        try_as_root curl -o "$user_home_bin/podman-remote-static-linux_${arch}.tar.gz" https://github.com/containers/podman/releases/download/v${PODMAN_v}/podman-remote-static-linux_${arch}.tar.gz
+        try_as_root tar -zxvf "$user_home_bin/podman-remote-static-linux_${arch}.tar.gz" -C "$user_home_local" bin/podman-remote-static-linux_${arch}
+        [ -f "$user_home_bin/podman-remote-static-linux_${arch}.tar.gz" ] && try_as_root rm -f "$user_home_bin/podman-remote-static-linux_${arch}.tar.gz"
         try_as_root mv "$user_home_bin/podman-remote-static-linux_${arch}" "$user_home_bin/podman"
         try_as_root ln -s "$user_home_bin/podman" "$user_home_bin/docker"
         fix_user_home "$user"
